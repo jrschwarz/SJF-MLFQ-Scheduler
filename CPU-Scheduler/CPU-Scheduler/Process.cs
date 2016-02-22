cb2e8b490839de9c9d28fd91a5829a5a18011418
Joey Schwarz <Joey_Schwarz@ultimatesoftware.com>
2016-02-22 09:57:25 -0500

    Added to Proccess Class

--- CPU-Scheduler/CPU-Scheduler/Process.cs
+++ CPU-Scheduler/CPU-Scheduler/Process.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +10,36 @@ namespace CPU_Scheduler
     class Process
     {
         private int[] _processes;
+        private int _index;
+
+
+        public int? curCPUTime;
+        public int ArrivalTime { get; set; }
+        public int? ResponseTime { get; set; }
+        public bool IsFinished { get; set; }
+        public Status State { get; set; }
+        public enum Status
+        {
+            READY,
+            RUNNING,
+            BLOCKED
+        }; 
 
         public Process()
         {
             _processes = new int[] { }; // initialize with no processes
+            ArrivalTime = 0;
+            ResponseTime = null;
+            IsFinished = false;
+            State = Status.READY;
+            _index = 0;
+            curCPUTime = null;
+
         }
         public void AddProcesses(int[] inputProcesses)
         {
             _processes = inputProcesses;
+            curCPUTime = _processes[_index];
         }
 
     }
